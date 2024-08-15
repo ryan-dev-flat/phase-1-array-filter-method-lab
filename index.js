bbd30d7173ef553544b3881e0db1e1e536591c86
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-08-14 22:03:14 -0600

    Update

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+const drivers = [
+    {
+        name: 'Bobby',
+        hometown: 'Pittsburgh'
+    },
+    {
+        name: 'Sammy',
+        hometown: 'New York'
+    },
+    {
+        name: 'Sally',
+        hometown: 'Cleveland'
+    },
+    {
+        name: 'Annette',
+        hometown: 'Los Angeles'
+    },
+    {
+        name: 'Bobby',
+        hometown: 'Tampa Bay'
+    }
+];
+
+function findMatching(drivers, query) {
+    query = query.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === query);
+}
+
+function fuzzyMatch(drivers, query) {
+    query = query.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query));
+}
+
+function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+}
